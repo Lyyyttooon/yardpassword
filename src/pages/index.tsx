53c060d00ab039e954707d55c532d2002852c72b
Lyyyttooon <bnn0202@outlook.com>
2023-04-28 08:35:39 +0800

    feat: 增加title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import PasswordGenerator from './password-generator';
 
 export default function Index() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PasswordGenerator></PasswordGenerator>
-    </main>
+    <>
+      <Head>
+        <title>YardPassword</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <PasswordGenerator></PasswordGenerator>
+      </main>
+    </>
   );
 }
