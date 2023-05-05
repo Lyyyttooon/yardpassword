de6b633f2af70fd66d38a23b14c38806504b5f18
Lyyyttooon <bnn0202@outlook.com>
2023-05-05 10:02:18 +0800

    feat: 增加特殊字符样式

--- src/components/password-text/index.tsx
+++ src/components/password-text/index.tsx
@@ -14,16 +14,26 @@ export default function PasswordText(props: Props) {
   function textStyle() {
     const items: JSX.Element[] = [];
     for (let i = 0; i < text.length; i++) {
-      items.push(spanStyle(text[i]));
+      items.push(spanStyle(text[i], i));
     }
     setStyleText(items);
   }
 
-  function spanStyle(i: string) {
+  function spanStyle(i: string, id: number) {
     if (reserveChar.number.includes(i)) {
-      return <span style={{ color: '#00e0e0' }}>{i}</span>;
+      return (
+        <span key={id} style={{ color: '#1677ff' }}>
+          {i}
+        </span>
+      );
+    } else if (reserveChar.specialCharacter.includes(i)) {
+      return (
+        <span key={id} style={{ color: '#d9363e' }}>
+          {i}
+        </span>
+      );
     } else {
-      return <span>{i}</span>;
+      return <span key={id}>{i}</span>;
     }
   }
 
