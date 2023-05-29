dc18d1e56994a63339183e5fcdcf78f639667952
Lyyyttooon <bnn0202@outlook.com>
2023-05-29 22:38:13 +0800

    feat: 添加样式

--- src/components/password-item/index.tsx
+++ src/components/password-item/index.tsx
@@ -1,9 +1,22 @@
 import { item } from '@/components/password-items';
 
+import { ScheduleOutlined } from '@ant-design/icons';
+
 interface Props {
   item: item;
 }
 
 export default function PasswordItem(props: Props) {
-  return <div>{props.item.name}</div>;
+  return (
+    <div className="flex justify-between w-full px-[3px]">
+      <div className="text-center leading-loose">
+        <ScheduleOutlined />
+        <span className="ml-[3px]">{props.item.name}</span>
+      </div>
+      <div>
+        <span>复制</span>
+        <span className="ml-[3px]">更多</span>
+      </div>
+    </div>
+  );
 }
